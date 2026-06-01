0f38255eb7094bc57f032a94cfa315c6d9b8ec8e
mamartam <marta.masiukk@gmail.com>
2026-06-01 15:19:36 +0300

    update app file and connect WorkPanel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import NavigationTree from "./components/NavigationTree";
+import WorkPanel from "./components/WorkPanel";
 import data from "./data";
 
 function App() {
+  const [content, setContent] = useState("Choose Note");
   return (
     <>
-      <NavigationTree data={data} />
+      <header>
+        <h1>Tree Notes</h1>
+      </header>
+      <main>
+        <NavigationTree data={data} onNoteSelect={setContent} />
+        <WorkPanel content={content} />
+      </main>
     </>
   );
 }
